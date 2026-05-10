7bb454623af5fec783d45d1edef8e3ee424a375f
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 17:54:27 +0700

    29

--- bai29.cpp
+++ bai29.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        // Xử lý overflow
-        if (dividend == INT_MIN && divisor == -1)
+        // Xử lý trường hợp tràn số duy nhất: -2^31 / -1 = 2^31 (vượt quá dải int)
+        if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
+        }
 
-        // Xác định dấu
-        bool negative = (dividend < 0) ^ (divisor < 0);
+        // Xác định dấu của kết quả
+        bool negative = (dividend > 0) ^ (divisor > 0);
 
-        long long dvd = abs((long long)dividend);
-        long long dvs = abs((long long)divisor);
+        // Chuyển sang số dương (dùng long long để tránh tràn số khi chuyển INT_MIN)
+        long long a = abs((long long)dividend);
+        long long b = abs((long long)divisor);
+        long long quotient = 0;
 
-        int result = 0;
-
-        while (dvd >= dvs) {
-            long long temp = dvs;
-            int multiple = 1;
-
-            // Nhân đôi divisor
-            while (dvd >= (temp << 1)) {
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+            // Tìm bội số lớn nhất của b (theo lũy thừa 2) mà vẫn <= a
+            while (a >= (temp << 1)) {
                 temp <<= 1;
                 multiple <<= 1;
             }
-
-            dvd -= temp;
-            result += multiple;
+            // Trừ bội số đó khỏi a và cộng số lần tương ứng vào thương
+            a -= temp;
+            quotient += multiple;
         }
 
-        return negative ? -result : result;
+        return negative ? -quotient : quotient;
     }
 };
