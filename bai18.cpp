215a808e0764eceb1b251ae9f8d227e95c3f7291
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 16:07:24 +0700

    18

---
+++ bai18.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+       vector<vector<int>> result;
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+
+    for (int i = 0; i < n - 3; i++) {
+        // Bỏ trùng i
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        for (int j = i + 1; j < n - 2; j++) {
+            // Bỏ trùng j
+            if (j > i + 1 && nums[j] == nums[j - 1])
+                continue;
+
+            int left = j + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum == target) {
+                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                    // Bỏ trùng left & right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < target) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1,0,-1,0,-2,2};
+    int target = 0;
+
+    vector<vector<int>> ans = fourSum(nums, target);
+
+    for (auto v : ans) {
+        cout << "[";
+        for (int x : v) cout << x << " ";
+        cout << "] ";
+    }
+
+    return 0;
+    }
+};
