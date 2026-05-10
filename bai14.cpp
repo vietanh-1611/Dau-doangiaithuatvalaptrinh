b3df469dbb9283ec269a4a8f8cec9026720f7b53
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 16:04:56 +0700

    14

---
+++ bai14.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       if (strs.empty()) return "";
+
+    string prefix = strs[0];  // lấy chuỗi đầu làm tiền tố
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) { 
+            prefix.pop_back();  // xóa ký tự cuối
+            if (prefix.empty()) return "";
+        }
+    }
+
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower","flow","flight"};
+    cout << longestCommonPrefix(strs);
+    return 0; 
+    }
+};
