da4cb8923fa00bd9d416e34651a087b0411dddbb
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 16:11:36 +0700

    26

---
+++ bai26.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+
+        int i = 0; // vị trí phần tử duy nhất
+
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1; // số phần tử duy nhất
+    }
+};
