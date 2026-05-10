7bdd4b509f0366029c4a99c54307c273feb2dc70
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 17:56:18 +0700

    30

--- bai30.cpp
+++ bai30.cpp
@@ -2,37 +2,50 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> result;
-
-        if (s.empty() || words.empty()) return result;
-
+        int n = s.length();
+        int numWords = words.size();
         int wordLen = words[0].length();
-        int wordCount = words.size();
-        int totalLen = wordLen * wordCount;
+        int totalLen = numWords * wordLen;
 
-        unordered_map<string, int> wordMap;
+        if (n < totalLen) return result;
 
-        // Đếm số lần xuất hiện của mỗi word
-        for (string w : words) {
-            wordMap[w]++;
+        // Đếm tần suất các từ mục tiêu
+        unordered_map<string, int> wordCounts;
+        for (const string& w : words) {
+            wordCounts[w]++;
         }
 
-        for (int i = 0; i <= (int)s.length() - totalLen; i++) {
-            unordered_map<string, int> seen;
-            int j = 0;
+        // Chạy cửa sổ trượt bắt đầu từ mỗi offset từ 0 đến wordLen - 1
+        for (int i = 0; i < wordLen; i++) {
+            int left = i;
+            int count = 0;
+            unordered_map<string, int> windowCounts;
 
-            while (j < wordCount) {
-                string word = s.substr(i + j * wordLen, wordLen);
+            for (int j = i; j <= n - wordLen; j += wordLen) {
+                string word = s.substr(j, wordLen);
+                
+                if (wordCounts.count(word)) {
+                    windowCounts[word]++;
+                    count++;
 
-                if (wordMap.find(word) == wordMap.end()) break;
+                    // Nếu từ này xuất hiện nhiều hơn số lượng cho phép, trượt 'left' sang phải
+                    while (windowCounts[word] > wordCounts[word]) {
+                        string leftWord = s.substr(left, wordLen);
+                        windowCounts[leftWord]--;
+                        count--;
+                        left += wordLen;
+                    }
 
-                seen[word]++;
-                if (seen[word] > wordMap[word]) break;
-
-                j++;
-            }
-
-            if (j == wordCount) {
-                result.push_back(i);
+                    // Nếu tìm đủ số lượng từ
+                    if (count == numWords) {
+                        result.push_back(left);
+                    }
+                } else {
+                    // Từ không nằm trong danh sách, reset cửa sổ
+                    windowCounts.clear();
+                    count = 0;
+                    left = j + wordLen;
+                }
             }
         }
 
