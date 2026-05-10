3ba340ba84ed42829b47f5971c3702d38f548e01
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 16:08:30 +0700

    20

---
+++ bai20.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            // Nếu là dấu mở
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                // Nếu stack rỗng -> sai
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                // Kiểm tra có khớp không
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return st.empty(); // Nếu rỗng -> hợp lệ
+    }
+};
