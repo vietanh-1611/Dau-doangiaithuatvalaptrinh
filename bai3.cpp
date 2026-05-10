d02f2a4a544c403f563df3ef4d6d1919b0ba5f44
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-10 14:39:09 +0700

    update bai3

---
+++ bai3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0); // node giả
+        ListNode* curr = dummy;
+        int carry = 0;
+
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int sum = carry;
+
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummy->next;
+    } 
+
+};
