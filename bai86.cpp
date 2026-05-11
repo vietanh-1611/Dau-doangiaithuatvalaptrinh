021056b5017b5341514e2dc0419b5341cdfad1f8
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:49:37 +0700

    86

---
+++ bai86.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while (left < right) {
+            // Hoán đổi hai ký tự ở hai đầu
+            swap(s[left], s[right]);
+            
+            // Di chuyển hai con trỏ lại gần nhau hơn
+            left++;
+            right--;
+        }
+    }
+};
