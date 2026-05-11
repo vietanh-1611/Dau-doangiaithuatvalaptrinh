e05cb8e635dd303ebeef234ad1f8818e4fcbb1e0
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:01:08 +0700

    01

--- bai01.cpp
+++ bai01.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp; // value -> index
-
-    for (int i = 0; i < nums.size(); i++) {
-        int canTim = target - nums[i];
-
-        // Nếu đã tồn tại số cần tìmgit status
-        if (mp.count(canTim)) {
-            return {mp[canTim], i};
+    bool containsDuplicate(vector<int>& nums) {
+        // Tạo một tập hợp không thứ tự để lưu các số đã duyệt qua
+        unordered_set<int> seen;
+        
+        for (int x : nums) {
+            // Nếu tìm thấy x trong tập hợp 'seen'
+            // seen.count(x) trả về 1 nếu x tồn tại, 0 nếu không
+            if (seen.count(x)) {
+                return true;
+            }
+            // Nếu chưa thấy, thêm x vào tập hợp
+            seen.insert(x);
         }
-
-        // Lưu số hiện tại vào map
-        mp[nums[i]] = i;
-    }
-
-    return {}; // theo đề bài thì dòng này hầu như không chạy
+        
+        // Nếu duyệt hết cả mảng mà không thấy trùng lặp
+        return false;
     }
 };
