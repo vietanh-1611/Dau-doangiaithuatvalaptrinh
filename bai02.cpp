9241b7dc03d35d104312666e2d8a97100ff62915
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:02:37 +0700

    02

--- bai02.cpp
+++ bai02.cpp
@@ -1,39 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0); // node giả
-        ListNode* curr = dummy;
-        int carry = 0;
-
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-            int sum = carry;
-
-            if (l1 != NULL) {
-                sum += l1->val;
-                l1 = l1->next;
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        // Dự phòng bộ nhớ trước để tối ưu hiệu suất (không bắt buộc nhưng nên làm)
+        ans.reserve(2 * n); 
+        
+        // Duyệt qua nums 2 lần
+        for (int i = 0; i < 2; i++) {
+            for (int x : nums) {
+                ans.push_back(x);
             }
-
-            if (l2 != NULL) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            carry = sum / 10;
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
         }
-
-        return dummy->next;
-    } 
-
+        return ans;
+    }
 };
