428d6c0f4250db51d23ce9666cddb5e3786cb09d
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:18:23 +0700

    13

--- bai13.cpp
+++ bai13.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-       unordered_map<char, int> value = {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
+    string intToRoman(int num) {
+        vector<int> values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
         
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
+        vector<string> symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
 
-    int result = 0;
+        string result;
 
-    for (int i = 0; i < s.length(); i++) {
-        // Nếu ký tự hiện tại nhỏ hơn ký tự sau -> trừ
-        if (i < s.length() - 1 && value[s[i]] < value[s[i + 1]]) {
-            result -= value[s[i]];
-        } else {
-            result += value[s[i]];
+        for (int i = 0; i < values.size(); i++) {
+            while (num >= values[i]) {
+                result += symbols[i];
+                num -= values[i];
+            }
         }
-    }
 
-    return result;
-}
-
-int main() {
-    string s;
-    cin >> s;
-    cout << romanToInt(s);
-    return 0;  
+        return result;
     }
 };
