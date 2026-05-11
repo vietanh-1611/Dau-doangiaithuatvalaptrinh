4dd1f84f63609ea89f661cd6a03bd4820a10341c
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:37:56 +0700

    31

--- bai31.cpp
+++ bai31.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int zeros = 0;
+        int last = n - 1;
 
-        // Bước 1: tìm i
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        // Bước 2: nếu tìm thấy
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+        // Bước 1: Tìm số lượng số 0 cần nhân bản
+        // và xác định vị trí biên (last)
+        for (int i = 0; i <= last - zeros; i++) {
+            if (arr[i] == 0) {
+                // Trường hợp đặc biệt: số 0 nằm ngay sát biên không thể nhân bản
+                if (i == last - zeros) {
+                    arr[last] = 0; // Chép số 0 này vào cuối
+                    last--;
+                    break;
+                }
+                zeros++;
             }
-            swap(nums[i], nums[j]);
         }
 
-        // Bước 3: đảo phần sau
-        reverse(nums.begin() + i + 1, nums.end());
+        // Bước 2: Duyệt ngược từ cuối mảng cũ về đầu
+        int j = n - 1; // Con trỏ ghi vào mảng mới
+        for (int i = last - zeros; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[j--] = 0;
+                arr[j--] = 0;
+            } else {
+                arr[j--] = arr[i];
+            }
+        }
     }
 };
