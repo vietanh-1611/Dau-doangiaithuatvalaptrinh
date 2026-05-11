44896584d2e5eef83ef7376f24c237c89612d25f
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 12:00:34 +0700

    105

--- bai105.cpp
+++ bai105.cpp
@@ -1,2 +1,15 @@
-
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sumTotal = n * (n + 1) / 2;
+        int x = sqrt(sumTotal);
+        
+        // Kiểm tra xem bình phương của x có bằng tổng không
+        if (x * x == sumTotal) {
+            return x;
+        }
+        
+        return -1;
+    }
+};
 // MSSV/HoTen - Bai 105
