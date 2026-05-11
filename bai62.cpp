8884f6d6d1fb590d11ebf8fdf1ded4a93fd5c7b2
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:25:49 +0700

    62

---
+++ bai62.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        // Chuyển mảng sang tập hợp để tìm kiếm nhanh
+        unordered_set<int> s(nums.begin(), nums.end());
+        
+        // Vòng lặp kiểm tra và nhân đôi
+        while (s.count(original)) {
+            original *= 2;
+        }
+        
+        return original;
+    }
+};
