ebbcc67a620bc3033c98b98107af48e47e4f5226
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:31:11 +0700

    67

---
+++ bai67.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // Hàm phụ để tính tổng các chữ số
+    int getDigitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    int countEven(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            // Kiểm tra nếu tổng chữ số là số chẵn
+            if (getDigitSum(i) % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
