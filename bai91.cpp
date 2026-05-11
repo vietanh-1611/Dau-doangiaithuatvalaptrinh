7ac45c00e1ef45aae5894aef8ade70bf9a4800e2
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:58:52 +0700

    91

---
+++ bai91.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = ""; // Sử dụng chuỗi này như một ngăn xếp (stack)
+        
+        for (char c : s) {
+            // Nếu ký tự hiện tại giống ký tự cuối cùng trong 'res'
+            if (!res.empty() && c == res.back()) {
+                // Xóa ký tự cuối cùng (loại bỏ cặp trùng lặp)
+                res.pop_back();
+            } else {
+                // Thêm ký tự hiện tại vào cuối
+                res.push_back(c);
+            }
+        }
+        
+        return res;
+    }
+};
