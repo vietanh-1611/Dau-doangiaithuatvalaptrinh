c79ca4e2cfd966bd3e6ae79cb81c7fde1f0ec5d7
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:20:06 +0700

    57

---
+++ bai57.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        // Duyệt từ đầu đến kế cuối
+        for (int i = 0; i < s.length() - 1; i++) {
+            // Nếu tìm thấy 'b' đứng ngay trước 'a'
+            if (s[i] == 'b' && s[i + 1] == 'a') {
+                return false;
+            }
+        }
+        
+        // Nếu không có vi phạm nào, chuỗi hợp lệ
+        return true;
+    }
+};
