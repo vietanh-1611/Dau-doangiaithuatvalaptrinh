c45f1e3fb9de0740978913c16f1d0148c9926b0b
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:32:10 +0700

    68

---
+++ bai68.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+        for (const string& w : words) {
+            // Kiểm tra xem pref có xuất hiện tại vị trí 0 của từ w hay không
+            if (w.find(pref) == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
