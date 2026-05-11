1fc8facf3d6a00f566ef28a0743e8cce05780266
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 11:04:47 +0700

    98

---
+++ bai98.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            // So sánh phần tử giữa với phần tử cuối cùng của phạm vi hiện tại
+            if (nums[mid] > nums[right]) {
+                // Điểm nhỏ nhất nằm ở bên phải mid
+                left = mid + 1;
+            } else {
+                // mid có thể là điểm nhỏ nhất, hoặc điểm nhỏ nhất nằm bên trái mid
+                right = mid;
+            }
+        }
+        
+        // Sau khi vòng lặp kết thúc, left == right và trỏ vào phần tử nhỏ nhất
+        return nums[left];
+    }
+};
