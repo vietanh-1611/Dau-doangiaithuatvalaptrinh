4029bbfbbb3b73954c9846d47e9b5393c642c9da
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:05:59 +0700

    56

---
+++ bai56.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int res = 0;
+    int mostWordsFound(vector<string>& sentences) {
+        int maxWords = 0;
+        
+        for (const string& s : sentences) {
+            int spaces = 0;
+            // Duyệt qua từng ký tự trong câu để đếm khoảng trắng
+            for (char c : s) {
+                if (c == ' ') {
+                    spaces++;
+                }
+            }
+            
+            // Số từ = số khoảng trắng + 1
+            int currentWords = spaces + 1;
+            
+            // Cập nhật giá trị lớn nhất
+            if (currentWords > maxWords) {
+                maxWords = currentWords;
+            }
+        }
+        
+        return maxWords;
+    }
+};
