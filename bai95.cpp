decc41642b0958d2aa86a238028d23d6471277f4
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 11:02:13 +0700

    95

---
+++ bai95.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            // Cách tính mid này giúp tránh tràn số (overflow) so với (left + right) / 2
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid; // Tìm thấy mục tiêu
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1; // Tìm ở nửa bên phải
+            } 
+            else {
+                right = mid - 1; // Tìm ở nửa bên trái
+            }
+        }
+        
+        return -1; // Không tìm thấy
+    }
+};
