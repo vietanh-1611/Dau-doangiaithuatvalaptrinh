f6a952614da2d0b2b4a07106abf0c1266aa1a133
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:50:28 +0700

    87

---
+++ bai87.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;
+        
+        // Bước 1: Đưa tất cả các số khác 0 lên đầu mảng
+        for (int cur = 0; cur < nums.size(); cur++) {
+            if (nums[cur] != 0) {
+                nums[lastNonZeroFoundAt] = nums[cur];
+                lastNonZeroFoundAt++;
+            }
+        }
+        
+        // Bước 2: Lấp đầy khoảng trống còn lại bằng số 0
+        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
