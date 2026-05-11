20f5e50d56aee5ae6f0de891537d3e4800b0050b
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:21:21 +0700

    16

--- bai16.cpp
+++ bai16.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-    int n = nums.size();
-
-    int closestSum = nums[0] + nums[1] + nums[2]; // giá trị ban đầu
-
-    for (int i = 0; i < n - 2; i++) {
-        int left = i + 1;
-        int right = n - 1;
-
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-
-            // Cập nhật nếu gần target hơn
-            if (abs(sum - target) < abs(closestSum - target)) {
-                closestSum = sum;
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        // Bước 1: Đánh dấu các số đã xuất hiện
+        for (int i = 0; i < nums.size(); i++) {
+            // Lấy giá trị tuyệt đối vì số này có thể đã bị đánh dấu âm trước đó
+            int val = abs(nums[i]) - 1; 
+            
+            // Nếu số tại vị trí 'val' còn dương, đổi nó thành âm
+            if (nums[val] > 0) {
+                nums[val] = -nums[val];
             }
-
-            if (sum < target)
-                left++;
-            else if (sum > target)
-                right--;
-            else
-                return sum; // đúng target luôn
         }
-    }
-
-    return closestSum;
-}
-
-int main() {
-    vector<int> nums = {-1, 2, 1, -4};
-    int target = 1;
-    cout << threeSumClosest(nums, target);
-    return 0;
+        
+        // Bước 2: Tìm các vị trí vẫn còn số dương
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                // Vị trí i tương ứng với số i + 1 bị thiếu
+                result.push_back(i + 1);
+            }
+        }
+        
+        return result;
     }
 };
