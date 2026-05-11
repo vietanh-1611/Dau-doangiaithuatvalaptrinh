3d0468009f85094c603b1aa6a25ceb1edf831105
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:21:38 +0700

    58

---
+++ bai58.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        stringstream ss(title);
+        string word;
+        string result = "";
+        
+        while (ss >> word) {
+            // Bước 1: Chuyển toàn bộ từ về chữ thường
+            for (char &c : word) {
+                c = tolower(c);
+            }
+            
+            // Bước 2: Nếu độ dài > 2, viết hoa chữ cái đầu
+            if (word.length() > 2) {
+                word[0] = toupper(word[0]);
+            }
+            
+            // Bước 3: Thêm vào chuỗi kết quả
+            if (result != "") {
+                result += " ";
+            }
+            result += word;
+        }
+        
+        return result;
+    }
+};
