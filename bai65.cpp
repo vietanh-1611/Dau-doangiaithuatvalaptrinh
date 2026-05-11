0cec7986cc019515e9f3ecbb751334b7bf5b049e
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:29:14 +0700

    65

---
+++ bai65.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int operations = 0;
+        
+        // Vòng lặp chạy cho đến khi một trong hai số bằng 0
+        while (num1 > 0 && num2 > 0) {
+            if (num1 >= num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            operations++;
+        }
+        
+        return operations;
+    }
+};
