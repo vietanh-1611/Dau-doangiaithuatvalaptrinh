c118246730f70c68746b417ca81036f46545069f
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 11:01:22 +0700

    94

---
+++ bai94.cpp
@@ -0,0 +1,35 @@
+class MinStack {
+private:
+    stack<int> mainStack;
+    stack<int> minStack;
+
+public:
+    MinStack() {
+        // Khởi tạo đối tượng
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        
+        // Nếu minStack trống hoặc giá trị mới <= giá trị nhỏ nhất hiện tại
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        } else {
+            // Nếu không, giá trị nhỏ nhất vẫn là giá trị cũ
+            minStack.push(minStack.top());
+        }
+    }
+    
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
