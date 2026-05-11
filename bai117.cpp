0c7bd94165d52a6fbc2777cf8d7ed8e198e21ffc
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 18:25:01 +0700

    117

--- bai117.cpp
+++ bai117.cpp
@@ -1,2 +1,17 @@
-
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        helper(root, res);
+        return res;
+    }
+    
+    void helper(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+        
+        helper(node->left, res);  // Duyệt trái
+        res.push_back(node->val); // Duyệt gốc
+        helper(node->right, res); // Duyệt phải
+    }
+};
 // MSSV/HoTen - Bai 117
