39bee68e04317f8711d404075625132869630ba9
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 18:19:34 +0700

    108

--- bai108.cpp
+++ bai108.cpp
@@ -1,2 +1,22 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+      vector<int> cnt(256, 0); // đếm ký tự
+    int left = 0, maxLen = 0;
 
+    for (int right = 0; right < s.length(); right++) {
+        cnt[s[right]]++;
+
+        // Nếu ký tự bị trùng
+        while (cnt[s[right]] > 1) {
+            cnt[s[left]]--;
+            left++;
+        }
+
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;  
+    }
+};
 // MSSV/HoTen - Bai 108
