ce8a7d9fdbc86c079bdb59fefc1410bc8c16052d
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 11:57:26 +0700

    102

--- bai102.cpp
+++ bai102.cpp
@@ -1,2 +1,28 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        
+        while (left < right) {
+            // Tránh tràn số nguyên khi n rất lớn (2^31 - 1)
+            int mid = left + (right - left) / 2;
+            
+            if (isBadVersion(mid)) {
+                // Nếu mid hỏng, điểm hỏng đầu tiên có thể là mid 
+                // hoặc ở phía trước mid.
+                right = mid; 
+            } else {
+                // Nếu mid còn tốt, chắc chắn điểm hỏng đầu tiên ở sau mid.
+                left = mid + 1;
+            }
+        }
+        
+        // Khi kết thúc vòng lặp, left == right và trỏ vào phiên bản hỏng đầu tiên.
+        return left;
+    }
+};
 // MSSV/HoTen - Bai 102
