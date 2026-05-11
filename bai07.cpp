fe107a09b985b15fe35bceb8d25bf1995762003f
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 09:09:52 +0700

    07

--- bai07.cpp
+++ bai07.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int rev = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-
-            // Kiểm tra tràn số
-            if (rev > INT_MAX / 10 || rev < INT_MIN / 10)
-                return 0;
-
-            rev = rev * 10 + digit;
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // Bước 1: Đếm tần suất xuất hiện của mỗi số
+        unordered_map<int, int> counts;
+        for (int n : nums) {
+            counts[n]++;
         }
 
-        return rev;
+        // Bước 2: Dùng Min-Heap để lưu top k phần tử
+        // pair<tần suất, giá trị>
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+
+        for (auto const& [val, freq] : counts) {
+            min_heap.push({freq, val});
+            // Nếu heap vượt quá k, loại bỏ phần tử có tần suất thấp nhất
+            if (min_heap.size() > k) {
+                min_heap.pop();
+            }
+        }
+
+        // Bước 3: Chuyển kết quả từ heap sang vector
+        vector<int> result;
+        while (!min_heap.empty()) {
+            result.push_back(min_heap.top().second);
+            min_heap.pop();
+        }
+        return result;
     }
 };
