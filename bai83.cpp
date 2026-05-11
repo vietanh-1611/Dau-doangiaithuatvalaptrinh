c9439677a5094975753ba6989af8943d764d0ff0
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:47:29 +0700

    83

---
+++ bai83.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        // Bước 1: Tìm vị trí xuất hiện đầu tiên của ký tự ch
+        int index = -1;
+        for (int i = 0; i < word.length(); i++) {
+            if (word[i] == ch) {
+                index = i;
+                break; // Chỉ lấy lần xuất hiện ĐẦU TIÊN
+            }
+        }
+        
+        // Bước 2: Nếu tìm thấy, thực hiện đảo ngược đoạn prefix
+        if (index != -1) {
+            // reverse nhận vào iterator bắt đầu và iterator kết thúc (không bao gồm)
+            // Vì vậy ta dùng word.begin() + index + 1 để bao gồm cả ký tự ch
+            reverse(word.begin(), word.begin() + index + 1);
+        }
+        
+        return word;
+    }
+};
