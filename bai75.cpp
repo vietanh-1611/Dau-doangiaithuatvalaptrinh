8734e0c0a9651651cfe4787e48706755ddef100b
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:40:24 +0700

    75

---
+++ bai75.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        // Bước 1: Khởi tạo Max-Heap từ mảng gifts
+        priority_queue<int> pq(gifts.begin(), gifts.end());
+        
+        // Bước 2: Thực hiện thao tác trong k giây
+        while (k--) {
+            // Lấy món quà lớn nhất
+            int max_val = pq.top();
+            pq.pop();
+            
+            // Thay thế bằng căn bậc hai của chính nó
+            pq.push(sqrt(max_val));
+        }
+        
+        // Bước 3: Tính tổng số quà còn lại
+        long long totalRemaining = 0;
+        while (!pq.empty()) {
+            totalRemaining += pq.top();
+            pq.pop();
+        }
+        
+        return totalRemaining;
+    }
+};
