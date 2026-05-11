80b03ee6dba7b429670b84b46aea658fcf921a3c
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:37:44 +0700

    73

---
+++ bai73.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+        // Giả sử số đầu tiên là số gần 0 nhất
+        int ans = nums[0];
+        
+        for (int x : nums) {
+            // So sánh giá trị tuyệt đối (khoảng cách tới 0)
+            if (abs(x) < abs(ans)) {
+                ans = x;
+            } 
+            // Nếu khoảng cách bằng nhau, chọn số lớn hơn (số dương)
+            else if (abs(x) == abs(ans)) {
+                if (x > ans) {
+                    ans = x;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
