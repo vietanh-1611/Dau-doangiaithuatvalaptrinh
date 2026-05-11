c75df1394f5524a7de0f744b9e7ec467828f2567
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 18:19:08 +0700

    107

--- bai107.cpp
+++ bai107.cpp
@@ -1,2 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        // Khởi tạo giá mua thấp nhất là vô hạn và lợi nhuận bằng 0
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
 
+        for (int price : prices) {
+            // Cập nhật giá mua thấp nhất nếu gặp mức giá rẻ hơn
+            if (price < minPrice) {
+                minPrice = price;
+            } 
+            // Nếu bán vào hôm nay có lời hơn mức lời cũ, hãy cập nhật
+            else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+};
 // MSSV/HoTen - Bai 107
