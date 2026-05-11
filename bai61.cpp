59fd64b4a02b03f3192e1c21fbf18a59a71fd0a1
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:24:49 +0700

    61

---
+++ bai61.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+        if (nums.size() < 3) return 0; // Cần ít nhất 3 phần tử để có thể thỏa mãn
+        
+        int min_val = nums[0];
+        int max_val = nums[0];
+        
+        // Bước 1: Tìm min và max của mảng
+        for (int x : nums) {
+            if (x < min_val) min_val = x;
+            if (x > max_val) max_val = x;
+        }
+        
+        int count = 0;
+        // Bước 2: Đếm các phần tử nằm trong khoảng (min_val, max_val)
+        for (int x : nums) {
+            if (x > min_val && x < max_val) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
