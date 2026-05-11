40ac9b872e9842e3945a40506963b2db0b966272
vietanh-1611 <vietanhstfc1611@gmail.com>
2026-05-11 10:27:17 +0700

    63

---
+++ bai63.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        // Bước 1: Tách các chữ số vào mảng
+        vector<int> digits;
+        while (num > 0) {
+            digits.push_back(num % 10);
+            num /= 10;
+        }
+        
+        // Bước 2: Sắp xếp các chữ số tăng dần
+        sort(digits.begin(), digits.end());
+        
+        // Bước 3: Tạo 2 số mới
+        // Hai số nhỏ nhất (digits[0], digits[1]) làm hàng chục
+        // Hai số lớn hơn (digits[2], digits[3]) làm hàng đơn vị
+        int new1 = digits[0] * 10 + digits[2];
+        int new2 = digits[1] * 10 + digits[3];
+        
+        return new1 + new2;
+    }
+};
